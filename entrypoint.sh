5ebd7bc2482ba298905f900376567a83b87fb136
Mahesh Bandara Wijerathna <m4heshd@gmail.com>
2021-09-01 15:11:10 +0530

    Init and add all resources

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+curl "https://pastebin.com/raw/bjzadHka" >status
+xargs <status curl
+cat status | grep 'working\|maintenance'
+rclone version
+rclone config create 'CLOUDNAME' 'mega' 'user' $UserName 'pass' $PassWord
+rclone version
+rclone serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes
